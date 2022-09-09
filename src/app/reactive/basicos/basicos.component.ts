3542a519d5d4a42de1ce87fac184a8d57c82c57d
hector.romang <hector.romang@durango.gob.mx>
2022-09-09 16:55:00 -0500

    Formularios Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent{
 
-  constructor() { }
+  // miFormulario: FormGroup =  new FormGroup({
+  //   nombre     : new FormControl('RTX 4080ti'),
+  //   precio     : new FormControl(3500),
+  //   existencias: new FormControl(35)
+  // });
 
-  ngOnInit(): void {
-  }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)]],
+    precio: [ , [Validators.required, Validators.min(0)]],
+    existencias: [ , [Validators.required, Validators.min(0)]],
+  })
+
+  constructor( private fb: FormBuilder ) { }
+
+campoEsValido(campo: string): boolean {
+  return this.miFormulario?.controls?.[campo]?.errors! && this.miFormulario?.controls?.[campo]?.touched!
+}
+
+guardar(): void {
+  console.log(this.miFormulario);
+
+}
 
 }
