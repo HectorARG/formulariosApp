4c31a621d8425c52c948292374917448ce6fbb43
HectorRoman <hectorr9577@gmail.com>
2022-09-10 23:42:26 -0500

    Validacion Email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // TODO: temporalmente
+
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [null, [Validators.required, Validators.pattern( this.nombreApellidoPattern )]],
+    email: [null, [Validators.required, Validators.pattern( this.emailPattern )]]
+  })
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Hector Roman',
+      email: 'test1@test.com'
+    })
+
+  }
+
+  campoNoValido( campo: string ): any {
+    // this.miFormulario.controls[campo].errors
+    return this.miFormulario.get(campo)?.errors && this.miFormulario.get(campo)?.touched
+  }
+
+  validar(): void{
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
   }
 
 }
